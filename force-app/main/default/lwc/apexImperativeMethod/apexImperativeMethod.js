0c6701bd8e1c816e83bbff79c845b68516140f26
rajeevlohani <rajeevlohani123@gmail.com>
2019-02-12 22:01:54 +0530

    Add files via upload

---
+++ force-app/main/default/lwc/apexImperativeMethod/apexImperativeMethod.js
@@ -0,0 +1,19 @@
+import { LightningElement, track } from 'lwc';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
+
+export default class ApexImperativeMethod extends LightningElement {
+    @track contacts;
+    @track error;
+
+    handleLoad() {
+        getContactList()
+            .then(result => {
+                this.contacts = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.contacts = undefined;
+            });
+    }
+}
